2e2889c6effa5c90dbe708b0347fc2f9c144c624
Oscar Shijie Song <oscarshijiesong@gmail.com>
2026-02-05 14:20:00 -0800

    Adds login and registration pages
    Implements login and registration pages with email and password authentication.
    
    Introduces basic UI elements for user input and navigation between the two pages.

---
+++ valuables-app/lib/pages/register_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:valuables/auth/auth_service.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // Authentication service instance
+  final authService = AuthService();
+
+  // User personal email controller
+  final _emailController = TextEditingController();
+  // User password controller
+  final _passwordController = TextEditingController();
+  // Controller for ensuring the same password
+  final _confirmPasswordController = TextEditingController();
+
+  void signUp() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+    final confirmPassword = _confirmPasswordController.text;
+
+    // Confirm the password
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Password don't match")));
+      return;
+    }
+
+    try {
+      await authService.signUpWithEmailPassword(email, password);
+
+      Navigator.pop(context);
+    } catch (err) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("Error: $err")));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Sign Up")),
+      body: ListView(
+        children: [
+          TextField(controller: _emailController),
+          TextField(controller: _passwordController, obscureText: true),
+          TextField(controller: _confirmPasswordController, obscureText: true),
+          ElevatedButton(onPressed: signUp, child: const Text("Sign Up")),
+        ],
+      ),
+    );
+  }
+}
