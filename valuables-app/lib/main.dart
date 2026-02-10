2e28756f2081ff3032b5934d20040a1e5d0cf4b8
Annika Chan <annikac2006@gmail.com>
2026-02-09 18:03:35 -0800

    removed .env file in yaml, included public supabase keys into a configuration file instead

--- valuables-app/lib/main.dart
+++ valuables-app/lib/main.dart
@@ -13,8 +13,8 @@ Future<void> main() async {
   await dotenv.load(fileName: ".env");
   
   await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    url: dotenv.env['supabaseURL']!,
+    anonKey: dotenv.env['supabaseAnonKey']!,
   );
   
   runApp(MyApp());
