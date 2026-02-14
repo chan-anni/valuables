3890a6a5be4952f4e8e678ab11a4cd09c71fa55f
Oscar Shijie Song <oscarshijiesong@gmail.com>
2026-02-13 19:37:48 -0800

    refactor: Before updating with developing

--- valuables-app/lib/auth/auth_service.dart
+++ valuables-app/lib/auth/auth_service.dart
@@ -3,8 +3,10 @@
 /// Includes helper function to get authentication status.
 library;
 
+import "dart:async";
+
 import "package:supabase_flutter/supabase_flutter.dart";
-import 'package:google_sign_in/google_sign_in.dart';
+import "package:google_sign_in/google_sign_in.dart";
 
 class AuthService {
   final SupabaseClient _supabase = Supabase.instance.client;
@@ -34,6 +36,42 @@ class AuthService {
     String password,
   ) async {
     return await _supabase.auth.signUp(email: email, password: password);
+  }
+
+  Future<AuthResponse> signInWithGoogle() async {
+    const webClientId =
+        '398491837853-hvd35lt2rgjb0g4ui20ft8kqg0oa4bmm.apps.googleusercontent.com';
+
+    const iosClientId =
+        '398491837853-k279v0djfia5g0s9itnnnbumo2a24aab.apps.googleusercontent.com';
+
+    // Google sign in on Android will work without providing the Android
+    // Client ID registered on Google Cloud.
+
+    final GoogleSignIn signIn = GoogleSignIn.instance;
+
+    // At the start of your app, initialize the GoogleSignIn instance
+    unawaited(
+      signIn.initialize(clientId: iosClientId, serverClientId: webClientId),
+    );
+
+    // Perform the sign in
+    final googleAccount = await signIn.authenticate();
+    final googleAuthorization = await googleAccount.authorizationClient
+        .authorizationForScopes([]);
+    final googleAuthentication = googleAccount.authentication;
+    final idToken = googleAuthentication.idToken;
+    final accessToken = googleAuthorization?.accessToken;
+
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    return await _supabase.auth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
   }
 
   /// Sign out from the current session
