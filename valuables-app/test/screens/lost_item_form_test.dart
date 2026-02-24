181a1b2581e6bf97208dd45718f4190ba2ed317d
Bimal Brar <bimalbrar@Bimals-MacBook-Pro.local>
2026-02-24 13:43:47 -0800

    Adjusting imports to pass one final CI test in order to merge

--- valuables-app/test/screens/lost_item_form_test.dart
+++ valuables-app/test/screens/lost_item_form_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:valuables/screens/lost_item_form.dart';
-import 'package:valuables/screens/map_picker_screen.dart';
+// import 'package:valuables/screens/map_picker_screen.dart';
 
 // Mock the Google Maps platform channel to prevent MissingPluginException during tests
 void _mockMapChannel() {
